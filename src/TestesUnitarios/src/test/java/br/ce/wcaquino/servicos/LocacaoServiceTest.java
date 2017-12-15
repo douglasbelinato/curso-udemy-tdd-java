841de824627f55fe625bfd12c358bd438b0bc325
Douglas Belinato <douglas_belinato@hotmail.com>
2017-12-15 15:42:16 -0200

    Seção 2 8. Formas de dividir um teste

--- src/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -8,7 +8,9 @@ import static org.junit.Assert.assertThat;
 
 import java.util.Date;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -17,8 +19,11 @@ import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
 	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
-	public void teste() {
+	public void testeLocacao() {
 		// Cenário
 		Usuario usuario = new Usuario("Douglas");
 		Filme filme = new Filme("Star Wars VII - O despertar da força", 10, 4.5);
@@ -28,14 +33,14 @@ public class LocacaoServiceTest {
 		
 		// Verificação
 		//Assert.assertEquals(4.5, locacao.getValor(), 0.01);
-		assertThat(locacao.getValor(), is(equalTo(4.5)));
-		assertThat(locacao.getValor(), is(not(6.0)));
+		error.checkThat(locacao.getValor(), is(equalTo(5.5)));
+		error.checkThat(locacao.getValor(), is(not(6.0)));
 		
 		//Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
 		
 		// Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		assertThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(false));
 	}
 
 }

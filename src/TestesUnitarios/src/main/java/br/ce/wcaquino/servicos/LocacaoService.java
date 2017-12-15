6316644eb9f971b2d18dd79df0f670ca506e8738
Douglas Belinato <douglas_belinato@hotmail.com>
2017-12-15 11:13:38 -0200

    Sessão 2 3. Testando sem ferramenta 4. JUnit

--- src/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,9 +4,13 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -28,7 +32,18 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
+		// Cenário
+		Usuario usuario = new Usuario("Douglas");
+		Filme filme = new Filme("Star Wars VII - O despertar da força", 10, 4.5);
 		
+		// Ação
+		Locacao locacao = new LocacaoService().alugarFilme(usuario, filme);
+		
+		// Verificação
+		Assert.assertTrue(locacao.getValor() == 4.5);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
 }

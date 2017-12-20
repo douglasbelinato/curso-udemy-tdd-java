34ae18518c157b4c5008c0c3d7e6079e273898a5
Douglas Belinato <douglas_belinato@hotmail.com>
2017-12-20 15:18:50 -0200

    Seção 3 23. BuilderMaster

--- src/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -33,6 +33,7 @@ import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
 import br.ce.wcaquino.exceptions.LocadoraException;
 import br.ce.wcaquino.utils.DataUtils;
+import buildermaster.BuilderMaster;
 
 public class LocacaoServiceTest {
 	
@@ -160,6 +161,10 @@ public class LocacaoServiceTest {
 		// Verificação
 		//assertThat(locacao.getDataRetorno(), caiEm(Calendar.SUNDAY));
 		assertThat(locacao.getDataRetorno(), caiNumaSegunda());
+	}
+	
+	public static void main(String[] args) {
+		new BuilderMaster().gerarCodigoClasse(Locacao.class);
 	}
 
 }

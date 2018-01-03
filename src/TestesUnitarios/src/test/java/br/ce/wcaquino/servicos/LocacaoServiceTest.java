e80d1b9d9f6b5763539ddf488220e0af8b1e67de
Douglas Belinato <douglas_belinato@hotmail.com>
2018-01-03 16:47:07 -0200

    Seção 6 39. Testando métodos privados diretamente

--- src/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -37,6 +37,7 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
 import br.ce.wcaquino.daos.LocacaoDAO;
 import br.ce.wcaquino.entidades.Filme;
@@ -315,6 +316,21 @@ public class LocacaoServiceTest {
 		// Verificação
 		Assert.assertThat(locacao.getValor(), is(1.0));
 		PowerMockito.verifyPrivate(locacaoService).invoke("calcularValorLocacao", filmes);
+	}
+	
+	@Test
+	public void deveCalcularLocacao() throws Exception {
+		// Cenário
+		List<Filme> filmes = Arrays.asList(umFilme().agora());
+		
+		// Ação
+		// Classe Whitebox existe tanto no Mockito qto no PowerMock, porém, é
+		// a do PoweMock que permite executar o método privado de modo concreto.
+		Double valor = (Double) Whitebox.invokeMethod(locacaoService, "calcularValorLocacao", filmes);
+		
+		// Verificação
+		Assert.assertThat(valor, is(4.0));
+		
 	}
 	
 
